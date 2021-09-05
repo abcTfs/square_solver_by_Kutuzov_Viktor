46ec5f419be0f6ee1e8551fc8df26ae7bce68ce6
Kutuzov Viktor <73025662+abcTfs@users.noreply.github.com>
2021-09-06 00:18:45 +0400

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -1,84 +1,81 @@
 #include "Mylib.h" /* including library*/
 
-void checking_val(int quaniti)
+void checking_val (int quaniti)
 {
     if (quaniti!= 3)
         printf ("You entered the wrong values");
 }
 
-bool near_to_0(double val)
+int is_Equal(double x, double xref)
 {
-  return (fabs(val)<eps);
+    return (fabs (xref - x) < eps) ? 1 : 0;
 }
 
-void AssertTesting()
+
+
+void assert_testing ()
 {
     double x1 = NAN, x2 = NAN;
-    printf("Testing started\n");
-    assert(((fabs(solve_ss(1, 2, 1, &x1, &x2)) - 1) < eps));
-    assert(fabs(x1 + 1) < eps);
-    assert(fabs(solve_ss(1,-5,6,&x1,&x2) - 2) < eps);
-    assert(fabs(x1 - 3) < eps);
-    assert(fabs(x2 - 2) < eps);
-    assert(fabs(solve_ss(0,0,0,&x1,&x2) + 1) < eps);
-    printf("Testing finished, all is ok\n\n");
+    printf ("Testing started\n");
+    assert (is_Equal(solve_ss(1, 2, 1, &x1, &x2), 1) );
+    assert (is_Equal (x1, -1));
+    assert (is_Equal (solve_ss(1,-5,6,&x1,&x2), 2));
+    assert (is_Equal (x1, 3));
+    assert (is_Equal (x2, 2));
+    assert (is_Equal(solve_ss(0,0,0,&x1,&x2), -1));
+    printf ("Testing finished, all is ok\n\n");
 }
 
 
-void UnitTestForSquare(int numb, int qua, double a, double b, double c, double x1ref, double x2ref)
+void unit_test_for_square (int numb, int qua, double a, double b, double c, double x1ref, double x2ref)
 {
-     double x1 = NAN,x2 = NAN;
-     int nRoots = solve_ss(a, b, c, &x1, &x2);
+     double x1 = NAN, x2 = NAN;
+     int nRoots = solve_ss (a, b, c, &x1, &x2);
      switch (nRoots)
      {
         case 2:
 
-            if (!isEqual(nRoots, qua)||!isEqual(x1, x1ref)||!isEqual(x2, x2ref))
+            if (!is_Equal (nRoots, qua) || !is_Equal (x1, x1ref) ||!is_Equal (x2, x2ref))
 
                 {
-                    printf("Test number %d FAILED:\n nRoots = %d, x1 = %lg, x2 = %lg.\n Expected:\n x1 = %lg, x2 = %lg",numb, qua, x1,x2, x1ref, x2ref);
+                    printf ("Test number %d FAILED:\n nRoots = %d, x1 = %lg, x2 = %lg.\n Expected:\n x1 = %lg, x2 = %lg",numb, qua, x1,x2, x1ref, x2ref);
                 }
             break;
         case 1:
-            if (!isEqual(nRoots, qua)||!isEqual(x1, x1ref))
+            if (!is_Equal (nRoots, qua) || !is_Equal (x1, x1ref))
                 {
-                     printf("Test number %d FAILED:\n nRoots = %d, x1 = %lg.\n Expected:\n x1 = %lg ", numb, qua, x1, x1ref);
+                     printf ("Test number %d FAILED:\n nRoots = %d, x1 = %lg.\n Expected:\n x1 = %lg ", numb, qua, x1, x1ref);
 
                 }
             break;
         case 0:
-            if (!isEqual(nRoots, qua))
+            if (!is_Equal (nRoots, qua))
             {
-               printf("Test number %d FAILED:\n nRoots = %d\n", numb, qua);
+               printf ("Test number %d FAILED:\n nRoots = %d\n", numb, qua);
             }
             break;
         case -1:
-            if (!isEqual(nRoots, qua))
+            if (!is_Equal (nRoots, qua))
             {
-                printf("Test number %d FAILED:\n nRoots=%d\n", numb, qua);
+                printf ("Test number %d FAILED:\n nRoots=%d\n", numb, qua);
             }
             break;
         default:
-            printf("error");
+            printf ("error");
      }
 }
 
-int isEqual(double x, double xref)
+void run_unit_test()
 {
-    return (fabs(xref - x) < eps) ? 1 : 0;
-}
+    printf ("Unittest started\n");
 
-void RunUnitTest()
-{
-    printf("Unittest started\n");
-
-                     // You must enter ref solutions in descending order
-                     //   numb,    qua,       a,         b,        c,        x1ref,        x2ref
-    UnitTestForSquare(       1,      1,       1,         2,        1,           -1,           NAN);
-    UnitTestForSquare(       2,      2,       1,        -5,        6,            3,             2);
-    UnitTestForSquare(       3,      0,       2,         2,        2,          NAN,           NAN);
-    UnitTestForSquare(       4,     -1,       0,         0,        0,          NAN,           NAN);
-    UnitTestForSquare(       5,      1,       0,         2,        0,            0,           NAN);
+                        // You must enter ref solutions in descending order
+                        //   numb,    qua,       a,         b,        c,        x1ref,        x2ref
+    unit_test_for_square(       1,      1,       1,         2,        1,           -1,           NAN);
+    unit_test_for_square(       2,      2,       1,        -5,        6,            3,             2);
+    unit_test_for_square(       3,      0,       2,         2,        2,          NAN,           NAN);
+    unit_test_for_square(       4,     -1,       0,         0,        0,          NAN,           NAN);
+    unit_test_for_square(       5,      1,       0,         2,        0,            0,           NAN);
 
     printf("Unittest finished\n\n");
 
@@ -92,11 +89,11 @@ int solve_ss(double coef1, double coef2, double coef3, double* sol1, double* sol
     assert(isfinite(coef2));
     assert(isfinite(coef1));
 
-    if (near_to_0(coef1))
+    if (is_Equal (coef1, 0))
     {
-        if (near_to_0(coef2))
+        if (is_Equal (coef2, 0))
         {
-            if (near_to_0(coef3))
+            if (is_Equal (coef3, 0))
                 return INF_ROOTS;
             else
                 return NO_ROOTS;
@@ -107,9 +104,9 @@ int solve_ss(double coef1, double coef2, double coef3, double* sol1, double* sol
     }
     else
     {
-        if (near_to_0(coef2))
+        if (is_Equal (coef2, 0))
         {
-            if (near_to_0(coef3))
+            if (is_Equal (coef3, 0))
             {
               *sol1=0;
               return ONE_ROOT;
@@ -136,7 +133,7 @@ int solve_ss(double coef1, double coef2, double coef3, double* sol1, double* sol
              *sol2 = (-coef2 - sqdiscr) / (2*coef1);
              return TWO_ROOTS;
          }
-         else if (near_to_0(discr))
+         else if (is_Equal (discr, 0))
          {
             *sol1 = (-coef2 + sqdiscr) / (2*coef1);
             *sol2 = *sol1;
@@ -154,15 +151,15 @@ void printing_solutions (int numb, double x1, double x2)
     switch (numb)
     {
         case NO_ROOTS:
-            printf("0 solve");
+            printf("0 solution");
             break;
 
         case ONE_ROOT:
-            printf("1 solve: x = %lg", x1);
+            printf("1 solution: x = %lg", x1);
             break;
 
         case TWO_ROOTS:
-            printf("2 solves: x1 = %lg, x2=%lg", x1, x2);
+            printf("2 solutions: x1 = %lg, x2 = %lg", x1, x2);
             break;
 
         case INF_ROOTS:
@@ -173,5 +170,4 @@ void printing_solutions (int numb, double x1, double x2)
 
             break;
     }
-
  }
