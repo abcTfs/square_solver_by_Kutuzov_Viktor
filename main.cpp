4f5eb764f6d43c876d056fd7f337db8fab1c38ff
abcTfs <73025662+abcTfs@users.noreply.github.com>
2021-09-05 20:26:40 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/*#ifdef LEHA
+#define LEHA_ZASRANETZ
+#endif*/
+
+#include "Mylib.h"
+//#include LEHA_ZASRANETZ
+#include "func.cpp"
+
+//main function
+int main()
+{
+
+    printf("This programm solves square equations\n (c) Kutuzov Viktor, 2021\n\n");
+
+    // Testing my prog
+    AssertTesting();
+    RunUnitTest();
+
+    printf("input your coefficient\nEnter through the space without any signs\n");
+
+    //val declaration
+
+    double a = NAN;
+    double b = NAN;
+    double c = NAN;
+
+    //checking the entered values
+
+    int check = scanf("%lg %lg %lg", &a, &b, &c);
+    checking_val(check);
+
+    //val declaration
+
+    double x1 = NAN;
+    double x2 = NAN;
+
+    int nRoots = solve_ss( a, b, c, &x1, &x2);
+
+    printing_solutions (nRoots, x1, x2);
+
+    return 0;
+}
